e3f20b3f4e3d591be0b82fc593a18d201210300a
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-07-13 22:06:39 -0400

    fixed sizing issue with moving patty wagon to the right

--- js/app.js
+++ js/app.js
@@ -1,14 +1,18 @@
-// PLAYER 1 PATTY WAGON
 let pattyWagon1 = document.querySelector('.patty-wagon-1');
-let moveBy = 20;
+let pattyWagon2 = document.querySelector('.patty-wagon-2');
+let moveBy = 60;
 
 window.addEventListener('load', () => {
     pattyWagon1.style.position = 'absolute';
     pattyWagon1.style.left = 0;
     pattyWagon1.style.bottom = 0;
+    pattyWagon2.style.position = 'absolute';
+    pattyWagon2.style.left = 0;
+    pattyWagon2.style.bottom = 0;
 });
 
 document.addEventListener('keydown', (e) => {
+
     console.log(e);
     switch (e.key) {
         case 'a':
@@ -23,23 +27,6 @@ document.addEventListener('keydown', (e) => {
         case 's':
             pattyWagon1.style.bottom = parseInt(pattyWagon1.style.bottom) - moveBy + 'px';
             break;
-    }
-});
-
-
-// PLAYER 2 PATTY WAGON
-
-let pattyWagon2 = document.querySelector('.patty-wagon-2');
-
-window.addEventListener('load', () => {
-    pattyWagon2.style.position = 'absolute';
-    pattyWagon2.style.left = 0;
-    pattyWagon2.style.bottom = 0;
-});
-
-document.addEventListener('keydown', (e) => {
-    console.log(e);
-    switch (e.key) {
         case 'ArrowLeft':
             pattyWagon2.style.left = parseInt(pattyWagon2.style.left) - moveBy + 'px';
             break;
