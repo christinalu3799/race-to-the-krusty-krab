ad6a7c474d2f6c30159539319609e2d4c3098f43
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-07-14 15:49:38 -0400

    trying to implement alternative strategy for moving the patty wagons

--- js/app.js
+++ js/app.js
@@ -1,43 +1,77 @@
 let pattyWagon1 = document.querySelector('.patty-wagon-1');
 let pattyWagon2 = document.querySelector('.patty-wagon-2');
 let moveBy = 60;
+let player = {};
+let keys = {
+    ArrowUp: false,
+    ArrowDown: false,
+    ArrowRight: false, 
+    ArrowLeft: false,
+};
 
-window.addEventListener('load', () => {
-    pattyWagon1.style.position = 'absolute';
-    pattyWagon1.style.left = 0;
-    pattyWagon1.style.bottom = 0;
-    pattyWagon2.style.position = 'absolute';
-    pattyWagon2.style.left = 0;
-    pattyWagon2.style.bottom = 0;
-});
+document.addEventListener("keydown", pressOn);
+document.addEventListener("keyup", pressOff);
 
-document.addEventListener('keydown', (e) => {
-
-    console.log(e);
-    switch (e.key) {
-        case 'a':
-            pattyWagon1.style.left = parseInt(pattyWagon1.style.left) - moveBy + 'px';
-            break;
-        case 'd':
-            pattyWagon1.style.left = parseInt(pattyWagon1.style.left) + moveBy + 'px';
-            break;
-        case 'w':
-            pattyWagon1.style.bottom = parseInt(pattyWagon1.style.bottom) + moveBy + 'px';
-            break;
-        case 's':
-            pattyWagon1.style.bottom = parseInt(pattyWagon1.style.bottom) - moveBy + 'px';
-            break;
-        case 'ArrowLeft':
-            pattyWagon2.style.left = parseInt(pattyWagon2.style.left) - moveBy + 'px';
-            break;
-        case 'ArrowRight':
-            pattyWagon2.style.left = parseInt(pattyWagon2.style.left) + moveBy + 'px';
-            break;
-        case 'ArrowUp':
-            pattyWagon2.style.bottom = parseInt(pattyWagon2.style.bottom) + moveBy + 'px';
-            break;
-        case 'ArrowDown':
-            pattyWagon2.style.bottom = parseInt(pattyWagon2.style.bottom) - moveBy + 'px';
-            break;
+function playGame() {
+    console.log('inplay');
+    if(player.start){
+        window.requestAnimationFrame(playGame);
     }
-});
+    
+}
+function pressOn(e) { 
+    e.preventDefault();
+    keys[e.key] = true;
+    console.log(keys);
+}
+function pressOff(e) { 
+    e.preventDefault();
+    keys[e.key] = false;
+    console.log(keys);
+}
+function start() {
+    console.log('your game has started');
+    player.start = true;
+    window.requestAnimationFrame(playGame);
+
+}
+
+// window.addEventListener('load', () => {
+//     pattyWagon1.style.position = 'absolute';
+//     pattyWagon1.style.left = 0;
+//     pattyWagon1.style.bottom = 0;
+//     pattyWagon2.style.position = 'absolute';
+//     pattyWagon2.style.left = 0;
+//     pattyWagon2.style.bottom = 0;
+// });
+
+// document.addEventListener('keydown', (e) => {
+
+//     console.log(e);
+//     switch (e.key) {
+//         case 'a':
+//             pattyWagon1.style.left = parseInt(pattyWagon1.style.left) - moveBy + 'px';
+//             break;
+//         case 'd':
+//             pattyWagon1.style.left = parseInt(pattyWagon1.style.left) + moveBy + 'px';
+//             break;
+//         case 'w':
+//             pattyWagon1.style.bottom = parseInt(pattyWagon1.style.bottom) + moveBy + 'px';
+//             break;
+//         case 's':
+//             pattyWagon1.style.bottom = parseInt(pattyWagon1.style.bottom) - moveBy + 'px';
+//             break;
+//         case 'ArrowLeft':
+//             pattyWagon2.style.left = parseInt(pattyWagon2.style.left) - moveBy + 'px';
+//             break;
+//         case 'ArrowRight':
+//             pattyWagon2.style.left = parseInt(pattyWagon2.style.left) + moveBy + 'px';
+//             break;
+//         case 'ArrowUp':
+//             pattyWagon2.style.bottom = parseInt(pattyWagon2.style.bottom) + moveBy + 'px';
+//             break;
+//         case 'ArrowDown':
+//             pattyWagon2.style.bottom = parseInt(pattyWagon2.style.bottom) - moveBy + 'px';
+//             break;
+//     }
+// });
