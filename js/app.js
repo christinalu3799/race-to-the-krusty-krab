04afccd788f60326c9a6611d3ed94cd1d8a0457b
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-07-13 21:26:36 -0400

    made patty wagon move on P2 side of screen

--- js/app.js
+++ js/app.js
@@ -1,4 +1,26 @@
 let pattyWagon = document.querySelector('.patty-wagon');
-console.log(pattyWagon);
+let moveBy = 10;
 
+window.addEventListener('load', () => {
+    pattyWagon.style.position = 'absolute';
+    pattyWagon.style.left = 0;
+    pattyWagon.style.bottom = 0;
+});
 
+document.addEventListener('keydown', (e) => {
+    console.log(e.key);
+    switch (e.key) {
+        case 'ArrowLeft':
+            pattyWagon.style.left = parseInt(pattyWagon.style.left) - moveBy + 'px';
+            break;
+        case 'ArrowRight':
+            pattyWagon.style.left = parseInt(pattyWagon.style.left) + moveBy + 'px';
+            break;
+        case 'ArrowUp':
+            pattyWagon.style.bottom = parseInt(pattyWagon.style.bottom) + moveBy + 'px';
+            break;
+        case 'ArrowDown':
+            pattyWagon.style.bottom = parseInt(pattyWagon.style.bottom) - moveBy + 'px';
+            break;
+    }
+});
